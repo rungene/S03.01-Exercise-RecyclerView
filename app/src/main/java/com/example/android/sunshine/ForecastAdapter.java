87fe4e86e60111473f418037f9e6b8b521771ab8
rungene <rungenesamuel@gmail.com>
2018-01-01 11:15:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -0,0 +1,39 @@
+package com.example.android.sunshine;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by SAM on 1/1/2018.
+ */
+
+public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastAdapterViewHolder> {
+
+
+    private String [] mWeatherData;
+    class ForecastAdapterViewHolder extends RecyclerView.ViewHolder{
+
+
+        public ForecastAdapterViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+
+
+    @Override
+    public ForecastAdapter.ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ForecastAdapter.ForecastAdapterViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
