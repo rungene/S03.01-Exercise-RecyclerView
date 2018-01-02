48239759e447cb248c66b5893aa086d645539a72
rungene <rungenesamuel@gmail.com>
2018-01-02 21:21:50 +0300

    ForecasAdapter updates

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by SAM on 1/1/2018.
@@ -13,15 +14,18 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
     private String [] mWeatherData;
 
+
     public ForecastAdapter(String[] mWeatherData) {
         this.mWeatherData = mWeatherData;
     }
 
     class ForecastAdapterViewHolder extends RecyclerView.ViewHolder{
+        TextView mWeatherTextView;
 
 
         public ForecastAdapterViewHolder(View itemView) {
             super(itemView);
+            mWeatherTextView = (TextView)itemView.findViewById(R.id.tv_weather_data);
         }
     }
 
