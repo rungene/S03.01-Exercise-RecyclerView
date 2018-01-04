ccced64ec18f068ca217bc0728dbc08df32dc00c
rungene <rungenesamuel@gmail.com>
2018-01-04 21:02:16 +0300

    ForecasAdapter updates

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.sunshine;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -27,17 +29,28 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
             super(itemView);
             mWeatherTextView = (TextView)itemView.findViewById(R.id.tv_weather_data);
         }
+
     }
 
 
 
     @Override
-    public ForecastAdapter.ForecastAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public ForecastAdapter.ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        Context context = viewGroup.getContext();
+        int layoutIdForListItem = R.layout.forecast_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(layoutIdForListItem,viewGroup,shouldAttachToParentImmediately);
+        ForecastAdapterViewHolder viewHolder = new ForecastAdapterViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ForecastAdapter.ForecastAdapterViewHolder holder, int position) {
+
+
 
     }
 
